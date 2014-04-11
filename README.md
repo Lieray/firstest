e79b054d8162518157479226cb55771636a21c46
liuwei <william@louding.com>
2014-04-11 11:21:59 +0800

    add project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Lieray's first project.
-
+这是我根据《Node.js开发指南》第5章内容而制作的Microblog微博页面。
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+cookieSecret: 'microblogByLieray',
+db:'microblog',
+host:'localhost',
+port:27017
+};
